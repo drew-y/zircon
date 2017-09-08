f6cc6671e4c170e58dde4ab282f71af4101f2f4d
Andrew Youngwerth <drew@youngwerth.com>
2017-09-08 14:20:53 -0600

    Better error handling in parser

--- src/engine/parser.ts
+++ src/engine/parser.ts
@@ -1,34 +1,31 @@
 import YAML = require("yamljs");
 
-type ParserOutput = { metadata: object | null, body: string };
+interface MetaData {
+  layout: string;
+  [key: string]: any;
+}
+
+type ParserOutput = { metadata: MetaData, body: string };
 
 function parseYAMLString(yaml: string): object | null {
-  try {
-    return YAML.parse(yaml.replace(/-{3,}/gm, ""));
-  } catch (error) {
-    console.log("Error in YAML header:");
-    console.log(yaml);
-    return null;
-  }
+  return YAML.parse(yaml.replace(/-{3,}/gm, ""));
 }
 
 function extractYAML(opts: { start: number, end: number, burrito: string }): ParserOutput {
-  return {
-    body: opts.burrito.slice(opts.end).trim(),
-    metadata: parseYAMLString(opts.burrito.slice(opts.start, opts.end))
-  }
+  const metadata = parseYAMLString(opts.burrito.slice(opts.start, opts.end)) as { layout?: string, [key: string]: any } | null;
+  if (metadata === null) throw new Error("Invalid yaml header");
+  if (!(typeof metadata.layout === "string")) throw new Error("Layout property missing from yaml header");
+  return { body: opts.burrito.slice(opts.end).trim(), metadata: metadata as MetaData };
 }
 
 export function parse(burrito: string): ParserOutput {
   const regex = /^-{3,}[^-]+-{3,}/g;
   const trimmedBurrito = burrito.trim();
   const match = regex.exec(trimmedBurrito);
-  if (match) {
-    return extractYAML({
-      start: match.index,
-      end: regex.lastIndex,
-      burrito: trimmedBurrito
-    });
-  }
-  return { body: trimmedBurrito, metadata: null };
+  if (!match) throw new Error("Could not find yaml header");
+  return extractYAML({
+    start: match.index,
+    end: regex.lastIndex,
+    burrito: trimmedBurrito
+  });
 }
