c9bbc258c8da4d3d83ab0f49795a889e09e0f75b
Andrew Youngwerth <drew@youngwerth.com>
2017-09-14 15:31:53 -0600

    Bugfix

--- src/cli.ts
+++ src/cli.ts
@@ -7,11 +7,24 @@ import { Engine } from "./engine";
 const DEFUALT_INPUT_PATH = "./";
 const DEFUALT_OUTPUT_PATH = "./site";
 
+if (process.argv.length <= 2) {
+  const engine = new Engine(DEFUALT_INPUT_PATH, DEFUALT_OUTPUT_PATH);
+  engine.generate();
+  process.exit(0);
+}
+
 program
   .version("0.0.1")
   .arguments("[input] [output]")
-  .action((input?: string, output?: string) => {
-    const engine = new Engine(input || DEFUALT_INPUT_PATH, output || DEFUALT_OUTPUT_PATH);
+  .action((input: string, output?: string) => {
+    const engine = new Engine(input, output || DEFUALT_OUTPUT_PATH);
+    engine.generate();
+  });
+
+ program
+  .version("0.0.1")
+  .action((input: string, output?: string) => {
+    const engine = new Engine(DEFUALT_INPUT_PATH, DEFUALT_OUTPUT_PATH);
     engine.generate();
   });
 
