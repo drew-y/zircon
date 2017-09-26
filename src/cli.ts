04dc0231834ccc256b4be8c4bca8dfb8292f1e4d
Andrew Youngwerth <drew@youngwerth.com>
2017-09-25 22:14:50 -0600

    Update cli docs

--- src/cli.ts
+++ src/cli.ts
@@ -10,10 +10,12 @@ const DEFUALT_OUTPUT_PATH = "./site";
 program
   .version("0.0.1")
   .option("-s, --skip-static", "Skip copying the static directory")
-  .arguments("[input] [output]");
+  .usage("[options] [input] [output]")
+  .description("Simple static site generation");
 
 program
   .command("init <destination>")
+  .description("Initialize a new site")
   .action((destination: string) => {
     console.log("Initializing");
     fs.copy(__dirname + "/../example", destination);
