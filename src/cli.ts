c8530edabdb44e3a9e3afdaffea463f79c77edaa
Andrew Youngwerth <drew@youngwerth.com>
2017-11-28 17:11:36 -0700

    Watch command (WIP)

--- src/cli.ts
+++ src/cli.ts
@@ -9,7 +9,6 @@ const DEFUALT_OUTPUT_PATH = "./site";
 
 program
   .version("0.0.1")
-  .option("-s, --skip-static", "Skip copying the static directory")
   .usage("[options] [input] [output]")
   .description("Simple static site generation");
 
@@ -22,11 +21,44 @@ program
     process.exit(0);
   });
 
+program
+  .command("watch [input] [output]")
+  .option("-s, --skip-static", "Skip copying the static directory")
+  .description("Watch the directory for file changes")
+  .action((input?: string, output?: string) => {
+    console.log("watching")
+    console.log(input);
+    console.log(output);
+    // const engine = new Engine({
+    //   inputPath: input || DEFUALT_INPUT_PATH,
+    //   outPath: output || DEFUALT_OUTPUT_PATH,
+    //   skipStatic: program.skipStatic
+    // });
+    // engine.generate();
+  });
+
+program
+  .command("build [input] [output]")
+  .option("-s, --skip-static", "Skip copying the static directory")
+  .description("Build the burrito site")
+  .action((input?: string, output?: string) => {
+    console.log("building")
+    console.log(input);
+    console.log(output);
+    // const engine = new Engine({
+    //   inputPath: input || DEFUALT_INPUT_PATH,
+    //   outPath: output || DEFUALT_OUTPUT_PATH,
+    //   skipStatic: program.skipStatic
+    // });
+    // engine.generate();
+  });
+
+program
+  .command('*')
+  .action(function() {
+    console.log('Unknown Command: ' + program.args.join(' '))
+    program.help()
+  })
+
 program.parse(process.argv);
 
-const engine = new Engine({
-  inputPath: program.args[0] || DEFUALT_INPUT_PATH,
-  outPath: program.args[1] || DEFUALT_OUTPUT_PATH,
-  skipStatic: program.skipStatic
-});
-engine.generate();
