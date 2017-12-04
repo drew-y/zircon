5cf618e828914b2ce6df96203caa4feef6d97faf
Andrew Youngwerth <drew@youngwerth.com>
2017-12-04 12:11:00 -0700

    Better default cli behavior

--- src/cli.ts
+++ src/cli.ts
@@ -8,8 +8,10 @@ import { Engine } from "./engine";
 const DEFUALT_INPUT_PATH = "./";
 const DEFUALT_OUTPUT_PATH = "./site";
 
+let didExecuteSubCommand = false;
+
 program
-  .version("0.0.1")
+  .version(require('../package.json').version)
   .usage("[options] [input] [output]")
   .description("Simple static site generation");
 
@@ -19,7 +21,7 @@ program
   .action((destination: string) => {
     console.log("Initializing");
     fs.copySync(__dirname + "/../example", destination);
-    process.exit(0);
+    didExecuteSubCommand = true;
   });
 
 program
@@ -47,11 +49,12 @@ program
       .on("ready", () => console.log("Watching..."))
       .on("change", path => console.log(`File ${path} has been changed`))
       .on("change", (...args: any[]) => {
-        console.log("Building");
+        console.log("Building...");
         engine.generate();
       });
 
     engine.generate();
+    didExecuteSubCommand = true;
   });
 
 program
@@ -59,24 +62,26 @@ program
   .option("-s, --skip-static", "Skip copying the static directory")
   .description("Build the burrito site")
   .action((input?: string, output?: string) => {
+    console.log("Building...");
     const engine = new Engine({
       inputPath: input || DEFUALT_INPUT_PATH,
       outPath: output || DEFUALT_OUTPUT_PATH,
       skipStatic: program.skipStatic
     });
     engine.generate();
-  });
-
-program
-  .command('*')
-  .action((input?: string, output?: string) => {
-    const engine = new Engine({
-      inputPath: input || DEFUALT_INPUT_PATH,
-      outPath: output || DEFUALT_OUTPUT_PATH,
-      skipStatic: program.skipStatic
-    });
-    engine.generate();
+    didExecuteSubCommand = true;
   });
 
 program.parse(process.argv);
+
+// No command picked. Build by default.
+if (!didExecuteSubCommand) {
+  console.log("Building...");
+  const engine = new Engine({
+    inputPath: program.args[0] || DEFUALT_INPUT_PATH,
+    outPath: program.args[1] || DEFUALT_OUTPUT_PATH,
+    skipStatic: program.skipStatic
+  });
+  engine.generate();
+}
 
