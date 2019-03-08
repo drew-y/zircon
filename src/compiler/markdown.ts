2e4bef75866a35d8d0dc8d7890b9b4bf4d7f8779
Drew Youngwerth <drew@youngwerth.com>
2019-03-08 14:25:56 -0700

    Replace remarkable with markdown-it

--- src/compiler/markdown.ts
+++ src/compiler/markdown.ts
@@ -1,6 +1,5 @@
-import Remarkable = require("remarkable");
+import MarkdownIt = require("markdown-it");
 import hljs = require("highlight.js");
-const toc = require("markdown-toc");
 
 // Syntax highlighter function for Remarkable
 function highlight(str: string, lang: string) {
@@ -18,15 +17,7 @@ function highlight(str: string, lang: string) {
 }
 
 // Initialize remarkable
-const md = new Remarkable({ highlight, html: true });
-
-// Register plugins
-md
-  // Add slugified id attribute to each header (Enables table of contents)
-  .use(function(remarkable: any) {
-    remarkable.renderer.rules.heading_open = function(tokens: any, idx: any) {
-      return `<h${tokens[idx].hLevel} id=${toc.slugify(tokens[idx + 1].content)} >`;
-    };
-  });
+const md = new MarkdownIt({ highlight, html: true });
+md.use(require("markdown-it-named-headings"));
 
 export { md };
